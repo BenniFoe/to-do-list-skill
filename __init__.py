fcc3f4172599dcf824c9bdb5067f3479796e8f9c
Benjamin <benjamin.foerster@stud-mail.uni-wuerzburg.de>
2022-06-23 16:07:01 +0200

    Test

--- __init__.py
+++ __init__.py
@@ -14,6 +14,10 @@ class ToDoList(MycroftSkill):
     # def handle_list_do_to(self, message):
     #     self.speak_dialog('list.do.to')
 
+    @intent_handler('test.intent')
+    def handle_test_intent(self):
+        self.speak_dialog('lelelelelelelelelelelel')
+
     @intent_handler('add.simply.one.bullet.point.intent')
     def handle_add_one_bullet_point(self):
         answer = self.get_response('what.is.the.bullet.point', num_retries=0)
