ce32412a8e6105f8a29992d1527eddf48bc8f84c
Benjamin <benjamin.foerster@stud-mail.uni-wuerzburg.de>
2022-06-23 16:09:54 +0200

    Test

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_handler
-from classes.BulletPointClass import BulletPoint
+#from classes.BulletPointClass import BulletPoint
 
 
 class ToDoList(MycroftSkill):
@@ -18,18 +18,18 @@ class ToDoList(MycroftSkill):
     def handle_test_intent(self):
         self.speak('lelelelelelelelelelelel')
 
-    @intent_handler('add.simply.one.bullet.point.intent')
-    def handle_add_one_bullet_point(self):
-        answer = self.get_response('what.is.the.bullet.point', num_retries=0)
-        bp = BulletPoint(name=answer)
-        self.bullet_points.append(bp)
-        self.speak_dialog('bullet.point.added')
-
-    @intent_handler('read.all.bullet.points.intent')
-    def handle_read_all_bullet_points(self):
-        count = 1
-        for bp in self.bullet_points:
-            self.speak_dialog('read.bullet.point.name', {'position': count, 'content': bp.get_name()})
+    # @intent_handler('add.simply.one.bullet.point.intent')
+    # def handle_add_one_bullet_point(self):
+    #     answer = self.get_response('what.is.the.bullet.point', num_retries=0)
+    #     bp = BulletPoint(name=answer)
+    #     self.bullet_points.append(bp)
+    #     self.speak_dialog('bullet.point.added')
+    #
+    # @intent_handler('read.all.bullet.points.intent')
+    # def handle_read_all_bullet_points(self):
+    #     count = 1
+    #     for bp in self.bullet_points:
+    #         self.speak_dialog('read.bullet.point.name', {'position': count, 'content': bp.get_name()})
 
 
 
