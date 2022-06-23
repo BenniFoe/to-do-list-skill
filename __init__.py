cd9b43ed60916137e42356b6a3e091b71a5ba943
Benjamin <benjamin.foerster@stud-mail.uni-wuerzburg.de>
2022-06-23 15:56:25 +0200

    Test

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from mycroft import MycroftSkill, intent_handler
+from mycroft import MycroftSkill, intent_file_handler
 from BulletPointClass import BulletPoint
 
 
@@ -14,14 +14,14 @@ class ToDoList(MycroftSkill):
     # def handle_list_do_to(self, message):
     #     self.speak_dialog('list.do.to')
 
-    @intent_handler('add.smiply.one.bullet.point.intent')
+    @intent_file_handler('add.simply.one.bullet.point.intent')
     def handle_add_one_bullet_point(self):
         answer = self.get_response('what.is.the.bullet.point', num_retries=0)
         bp = BulletPoint(name=answer)
         self.bullet_points.append(bp)
         self.speak_dialog('bullet.point.added')
 
-    @intent_handler('read.all.bullet.points.intent')
+    @intent_file_handler('read.all.bullet.points.intent')
     def handle_read_all_bullet_points(self):
         count = 1
         for bp in self.bullet_points:
